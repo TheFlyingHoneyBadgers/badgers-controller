1221d39e70c83e3a307f4d9431fc3fd64796bd01
Funniguy <liao.li@vanderbilt.edu>
2023-11-27 22:31:09 -0600

    update tgz files

---
+++ badgerscontroller/src/badgerscontroller_data.cpp
@@ -0,0 +1,98 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: badgerscontroller_data.cpp
+//
+// Code generated for Simulink model 'badgerscontroller'.
+//
+// Model version                  : 8.3
+// Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+// C/C++ source code generated on : Mon Nov 27 22:28:20 2023
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#include "badgerscontroller.h"
+
+// Block parameters (default storage)
+P_badgerscontroller_T badgerscontroller_P = {
+  // Mask Parameter: PIDController_D
+  //  Referenced by: '<S34>/Derivative Gain'
+
+  0.0,
+
+  // Mask Parameter: PIDController_I
+  //  Referenced by: '<S37>/Integral Gain'
+
+  0.0,
+
+  // Mask Parameter: PIDController_InitialConditionF
+  //  Referenced by: '<S35>/Filter'
+
+  0.0,
+
+  // Mask Parameter: PIDController_InitialConditio_k
+  //  Referenced by: '<S40>/Integrator'
+
+  0.0,
+
+  // Mask Parameter: PIDController_N
+  //  Referenced by: '<S43>/Filter Coefficient'
+
+  100.0,
+
+  // Mask Parameter: PIDController_P
+  //  Referenced by: '<S45>/Proportional Gain'
+
+  2.5,
+
+  // Computed Parameter: Constant_Value
+  //  Referenced by: '<S1>/Constant'
+
+  {
+    0.0                                // Data
+  },
+
+  // Computed Parameter: Out1_Y0
+  //  Referenced by: '<S6>/Out1'
+
+  {
+    0.0                                // Data
+  },
+
+  // Computed Parameter: Constant_Value_g
+  //  Referenced by: '<S3>/Constant'
+
+  {
+    0.0                                // Data
+  },
+
+  // Computed Parameter: Out1_Y0_i
+  //  Referenced by: '<S7>/Out1'
+
+  {
+    0.0                                // Data
+  },
+
+  // Computed Parameter: Constant_Value_f
+  //  Referenced by: '<S4>/Constant'
+
+  {
+    0.0                                // Data
+  },
+
+  // Expression: 3
+  //  Referenced by: '<S5>/Constant'
+
+  3.0
+};
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
---
+++ src/badgerscontroller/include/badgerscontroller/visibility_control.h
@@ -0,0 +1,32 @@
+#ifndef BADGERSCONTROLLER__VISIBILITY_CONTROL_H_
+#define BADGERSCONTROLLER__VISIBILITY_CONTROL_H_
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define BADGERSCONTROLLER_EXPORT __attribute__ ((dllexport))
+    #define BADGERSCONTROLLER_IMPORT __attribute__ ((dllimport))
+  #else
+    #define BADGERSCONTROLLER_EXPORT __declspec(dllexport)
+    #define BADGERSCONTROLLER_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef BADGERSCONTROLLER_BUILDING_LIBRARY
+    #define BADGERSCONTROLLER_PUBLIC BADGERSCONTROLLER_EXPORT
+  #else
+    #define BADGERSCONTROLLER_PUBLIC BADGERSCONTROLLER_IMPORT
+  #endif
+  #define BADGERSCONTROLLER_PUBLIC_TYPE BADGERSCONTROLLER_PUBLIC
+  #define BADGERSCONTROLLER_LOCAL
+#else
+  #define BADGERSCONTROLLER_EXPORT __attribute__ ((visibility("default")))
+  #define BADGERSCONTROLLER_IMPORT
+  #if __GNUC__ >= 4
+    #define BADGERSCONTROLLER_PUBLIC __attribute__ ((visibility("default")))
+    #define BADGERSCONTROLLER_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define BADGERSCONTROLLER_PUBLIC
+    #define BADGERSCONTROLLER_LOCAL
+  #endif
+  #define BADGERSCONTROLLER_PUBLIC_TYPE
+#endif
+#endif  // BADGERSCONTROLLER__VISIBILITY_CONTROL_H_
+// Generated 27-Nov-2023 22:28:29
+// Copyright 2019-2020 The MathWorks, Inc.
